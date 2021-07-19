c1d89cd1691458a2623c1a0ac154e27bfbe997a6
peter279k <peter279k@gmail.com>
2021-07-19 08:51:03 +0800

    Using different PHP versions to build phars

--- build.sh
+++ build.sh
@@ -1,36 +1,45 @@
 #!/bin/bash
 
+php_version=$1
+
 git clone https://github.com/Athlon1600/youtube-downloader
 
 cd youtube-downloader/
 
-composer install -n
+composer update -n
 curl -JL https://clue.engineering/phar-composer-latest.phar -o phar-composer.phar
 
 for tag_name in $(git tag)
 do
     if [[ ! -d "../$tag_name" ]]; then
         mkdir "../$tag_name"
+        if [[ ! -d "../$tag_name/php-$php_version" ]]; then
+            mkdir "../$tag_name/php-$php_version"
+        fi;
     fi;
     php ./phar-composer.phar build ./ 2>&1 > /dev/null
-    cp youtube-downloader.phar "../$tag_name"
+    cp youtube-downloader.phar "../$tag_name/php-$php_version"
     rm -f youtube-downloader.phar
     if [[ $? == 0 ]]; then
-        echo "Builidng $tag_name has been done."
+        echo "Builidng $tag_name with PHP-$php_version has been done."
     else
-        echo "Building $tag_name has been failed!"
+        echo "Building $tag_name with PHP-$php_version has been failed!"
     fi;
 done;
 
 tag_name="master"
 if [[ ! -d "../$tag_name" ]]; then
-        mkdir "../$tag_name"
+    mkdir "../$tag_name"
+    if [[ ! -d "../$tag_name/php-$php_version" ]]; then
+        mkdir "../$tag_name/php-$php_version"
+    fi;
 fi;
+
 php ./phar-composer.phar build ./ 2>&1 > /dev/null
-cp youtube-downloader.phar "../$tag_name"
+cp youtube-downloader.phar "../$tag_name/php-$php_version"
 rm -f youtube-downloader.phar
 if [[ $? == 0 ]]; then
-    echo "Builidng $tag_name has been done."
+    echo "Builidng $tag_name with PHP-$php_version has been done."
 else
-    echo "Building $tag_name has been failed!"
+    echo "Building $tag_name with PHP-$php_version has been failed!"
 fi;
